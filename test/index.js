644ddfb39e1e89ca5a56235430f12b6f655dedc3
zhaoxiang <13823394@qq.com>
2019-12-27 10:47:17 +0800

    修改出错时处理bug

--- test/index.js
+++ test/index.js
@@ -1,27 +1,35 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhaoxiang
+ * @Date: 2019-11-14 09:45:18
+ * @LastEditors  : zhaoxiang
+ * @LastEditTime : 2019-12-27 10:36:01
+ */
 const RedisSharding = require('../lib')
 
 const redisSharding = new RedisSharding(
-  ['127.0.0.1:16379', '127.0.0.1:16380']
+  JSON.parse("[{\"host\":\"192.168.175.12\",\"port\":13318},{\"host\":\"192.168.175.13\",\"port\":43102}]")
 );
 
-const client = redisSharding.getClient()
+const client = redisSharding.getProxy()
 
 async function test() {
   console.log(1);
-  await client.lpush('testlist', 'bar')
-  console.log(1);
-  await client.lpush('testlist', 'bar2')
-  console.log(1);
-  await client.lrange('testlist', 0, 10)
-  console.log(1);
-  await client.set('test1', 'bar2')
-  console.log(1);
-  await client.set('test2', 'bar2')
-  console.log(1);
-  await client.set('test3', 'bar2')
-  console.log(1);
-  await client.set('test4', 'bar2')
-  console.log(1);
+  // await client.lpush('testlist', 'bar', '1')
+  // console.log(1);
+  // await client.lpush('testlist', 'bar2')
+  // console.log(1);
+  // await client.lrange('testlist', 0, 10)
+  // console.log(1);
+  await client.set('test1')
+  // console.log(1);
+  // await client.set('test2', 'bar2')
+  // console.log(1);
+  // await client.set('test3', 'bar2')
+  // console.log(1);
+  // await client.set('test4', 'bar2')
+  // console.log(1);
   client.destroyAllClients();
 }
 
